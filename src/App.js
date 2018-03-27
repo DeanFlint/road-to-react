a42f034e513a013bb41275b049c31627271e6fa8
Dean Flint <dean.p.flint@gmail.com>
2018-03-27 09:38:29 +0000

    updates

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>Welcome to the Road to learn React </h2>
+      </div>
+    );
+  }
+}
+
+export default App;
