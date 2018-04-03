338ddb23081d2a346dcc8dc19a90c0adfe10a15f
Dean Flint <dean.p.flint@gmail.com>
2018-04-03 14:07:02 +0000

    style added, 3 stateless components created

--- src/index.js
+++ src/index.js
@@ -2,7 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+if (module.hot) {
+    module.hot.accept();
+}
+
+/*
+Hot Module Replacement
+Hot Module Replacement (HMR) is a tool to reload your application in the browser. 
+The browser doesn’t perform a page refresh. You can easily activate it in 
+create-react-app. In your src/index.js, your entry point to React, you have to 
+add one little configuration.
+
+The biggest benefit is that you can keep the application state with HMR.
+It keeps the application state even though the source code changes. 
+The application itself reloads, but not the page.
+*/
